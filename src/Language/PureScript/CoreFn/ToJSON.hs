152cafe75af919ceecbd36cbcc5deb4f4019187a
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2021-05-07 16:06:47 +0000

    Don't break CoreFN JSON parsing (discard IsSyntheticApp)

--- src/Language/PureScript/CoreFn/ToJSON.hs
+++ src/Language/PureScript/CoreFn/ToJSON.hs
@@ -38,7 +38,8 @@ metaToJSON IsNewtype              = object [ T.pack "metaType"  .= "IsNewtype" ]
 metaToJSON IsTypeClassConstructor = object [ T.pack "metaType"  .= "IsTypeClassConstructor" ]
 metaToJSON IsForeign              = object [ T.pack "metaType"  .= "IsForeign" ]
 metaToJSON IsWhere                = object [ T.pack "metaType"  .= "IsWhere" ]
-metaToJSON IsSyntheticApp         = object [ T.pack "metaType"  .= "IsSyntheticApp" ]
+metaToJSON IsSyntheticApp         = Null -- object [ T.pack "metaType"  .= "IsSyntheticApp" ]
+
 
 sourceSpanToJSON :: SourceSpan -> Value
 sourceSpanToJSON (SourceSpan _ spanStart spanEnd) =
